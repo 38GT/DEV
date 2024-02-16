90b7750534d536b20fa35eb13a0b240038a1fde1
38GT <gt38@gtfactory.io>
2024-02-16 09:55:51 +0900

    열심히 변경 중, class사용해서 설계하니깐 계속 지웠다 썼다 하네

--- DARTBOT/src/DART.js
+++ DARTBOT/src/DART.js
@@ -25,7 +25,7 @@ export class DART {
     DART.#today_list = today_list;
   }
 
-  static async start_today_polling(period, start = "", end = "") {
+  static async fetch_DART() {
     try {
       const response = await axios.get(
         `https://opendart.fss.or.kr/api/list.json?crtfc_key=${API_KEY}&page_count=10`
@@ -36,7 +36,7 @@ export class DART {
         response.data.total_count
       ) {
         const old_list = DART.today_list;
-        DART.today_list = await get_today_list(response);
+        DART.today_list = await DART.#get_today_list(response);
         DART.new_list = DART.today_list.filter(
           // 이 부분 map,set,obj 사용해보면서 성능 테스트 해보기
           (item) => !old_list.some((disc) => is_same_disclosure(disc, item))
@@ -44,34 +44,27 @@ export class DART {
       }
     } catch (err) {
       console.error("API 받아오기 실패", err);
-    } finally {
-      try {
-        setTimeout(() => DART.start_today_polling(period), period);
-      } catch (err) {
-        console.err("마지막 파이널리 구문에서 확인하는 에러 콘솔", err);
-      }
     }
+  }
+
+  static async #get_today_list(response) {
+    const pageRequests = [];
+    const pageNum = response.data.total_page;
+    for (let i = 1; i <= pageNum; i++) {
+      const pageRequest = axios
+        .get(
+          `https://opendart.fss.or.kr/api/list.json?crtfc_key=${API_KEY}&page_count=10&page_no=${i}`
+        )
+        .then((pageResponse) => pageResponse.data.list);
+      pageRequests.push(pageRequest);
+    }
+    const allPagesList = await Promise.all(pageRequests);
+    const todayList = [].concat(...allPagesList);
+    return [...new Set(todayList)];
   }
 }
 
-//한번에 요청하는 페이지 수 늘려서 API 호출 수 줄이기
-export const get_today_list = async (response) => {
-  const pageRequests = [];
-  const pageNum = response.data.total_page;
-  for (let i = 1; i <= pageNum; i++) {
-    const pageRequest = axios
-      .get(
-        `https://opendart.fss.or.kr/api/list.json?crtfc_key=${API_KEY}&page_count=10&page_no=${i}`
-      )
-      .then((pageResponse) => pageResponse.data.list);
-    pageRequests.push(pageRequest);
-  }
-  const allPagesList = await Promise.all(pageRequests);
-  const todayList = [].concat(...allPagesList);
-  return [...new Set(todayList)];
-};
-
-export function is_same_disclosure(disc1, disc2) {
+function is_same_disclosure(disc1, disc2) {
   return (
     disc1.corp_code === disc1.corp_code &&
     disc1.report_nm === disc2.report_nm &&
--- DARTBOT/src/app.js
+++ DARTBOT/src/app.js
@@ -18,10 +18,25 @@ const app = express();
 
 //뒤에 폴링 기간설정 로직 미추가 상태
 
-DART.start_today_polling(0.1 * oneMinute, "아침 9시부터", "저녁 10시까지");
+const queue = new Queue();
 
+// 다트와 폴링을 분리시켜야 하는가? 모르겠다. 다른 곳에서 해당 주기를 또 사용하지 않을 것 같다.
+// -> 폴링에 의존적인게 맞다.
+// 만약 사용한다면 나중에 합치면 되려나,
 // CHAT_BOT.start_work();
 
+polling_and_enqueue(queue);
+
+function polling_and_enqueue(queue) {
+  try {
+    DART.fetch_DART();
+    DART.new_list.forEach((data) => queue.enqueue(data));
+  } catch (err) {
+    console.err("DART 받아오기 실패", err); // 실패한 경우 DART.new_list 안에 에러메세지 같은게 들어 있으면 어떻게 하지? 이것도 확인해보자.
+  } finally {
+    setTimeout(() => polling_and_enqueue(queue), 0);
+  }
+}
 REPORT.find_news(DART.new_list) // 프라미스 객체를 리턴하는 함수여야 한다.
   .then((news) => REPORT.publish(news))
   .then((reports) => {
